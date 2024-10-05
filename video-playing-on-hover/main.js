170133bffa63105c92c3663b16bcdb184295f3c2
FedorovIV <fedorov.iv@phystech.edu>
2024-10-05 22:45:47 +0300

    first version

---
+++ video-playing-on-hover/main.js
@@ -0,0 +1,28 @@
+let currentVideoContainer;
+
+document.addEventListener('mouseover', (ev)=>{
+    const videoContainer = ev.target.closest('.video-container');
+    if (!videoContainer || currentVideoContainer) return;
+
+    currentVideoContainer = videoContainer;
+
+    const video = currentVideoContainer.querySelector('.video-container__video');
+    const image = currentVideoContainer.querySelector('.video-container__image');
+
+    video.play();
+    image.hidden = true;
+})
+
+document.addEventListener('mouseout', (ev)=>{
+    const videoContainer = ev.target.closest('.video-container');
+    if (!videoContainer || !currentVideoContainer) return;
+
+    const video = currentVideoContainer.querySelector('.video-container__video');
+    const image = currentVideoContainer.querySelector('.video-container__image');
+
+    video.pause();
+    video.currentTime = 0;
+    image.hidden = false;
+
+    currentVideoContainer = undefined;
+})
